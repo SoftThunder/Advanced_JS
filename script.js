24ca92de696aa7d1cc59d35e15747a8eae02e157
SoftThunder <grom1304@gmail.com>
2022-05-22 22:04:56 +0300

    введение классов

--- script.js
+++ script.js
@@ -1,23 +1,48 @@
 const goods = [
-    { title: 'Shirt', price: 150 },
-    { title: 'Socks', price: 50 },
-    { title: 'Jacket', price: 350 },
-    { title: 'Shoes', price: 250 },
+  { title: 'Shirt', price: 150 },
+  { title: 'Socks', price: 50 },
+  { title: 'Jacket', price: 350 },
+  { title: 'Shoes', price: 250 },
 ];
-
-const renderGoodsItem = (title = '', price = '') => {
+class GoodsItem {
+  constructor({ title = '', price = 0 }) {
+    this.title = title;
+    this.price = price;
+  }
+  render() {
     return `
-      <div class="goods-item">
-        <h3>${title}</h3> 
-        <p>${price}</p>
-      </div>
-    `;
-};
-
-const renderGoodsList = (list = []) => {
-    let goodsList = list.map(item => 
-    renderGoodsItem(item.title, item.price)).join('');
-    document.querySelector('.goods-list').innerHTML = goodsList;
+    <div class="goods-item">
+    <h3>${this.title}</h3> 
+    <p>${this.price}</p>
+  </div>
+    `
+  }
 }
 
-renderGoodsList(goods);
+class GoodsList {
+  constructor(list = []) {
+    this.list = list;
+  }
+  render() {
+    const goodsList = this.list.map(item => {
+      const goodsItem = new GoodsItem(item);
+      return  goodsItem.render();
+    }).join('');
+    document.querySelector('.goods-list').innerHTML = goodsList;
+  }
+//   sumPrice(){
+//   const initialValue = 0;
+//  const sumWithInitial = this.list.reduce(
+//   (previousValue.price, currentValue.price) => previousValue.price + currentValue.price,
+//   initialValue.price
+//  );
+// return sumWithInitial;
+//   }
+
+}
+
+const goodsList = new GoodsList(goods);
+goodsList.render();
+
+
+//console.log(sumWithInitial);
